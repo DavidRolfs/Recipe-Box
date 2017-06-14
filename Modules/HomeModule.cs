9a12aa5771aa5849fbe96b0ca902d2f5a1e9cd52
Alyssa Moody and David Rolfs <alyssa+david@>
2017-06-14 15:54:15 -0700

    add routing for recipes

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -30,7 +30,7 @@ namespace RecipeBox
       Post["/recipes/new"] = _ => {
         Recipe newRecipe = new Recipe(Request.Form["recipe-name"], Request.Form["recipe-rating"], Request.Form["recipe-instructions"]);
         newRecipe.Save();
-        return View["success.cshtml"];
+        return View["index.cshtml"];
       };
       Get["/categories/new"] = _ => {
         return View["category_form.cshtml"];
@@ -38,7 +38,7 @@ namespace RecipeBox
       Post["/categories/new"] = _ => {
         Category newCategory = new Category(Request.Form["category-name"]);
         newCategory.Save();
-        return View["success.cshtml"];
+        return View["index.cshtml"];
       };
       Get["/ingredients/new"] = _ => {
         return View["ingredient_form.cshtml"];
@@ -46,8 +46,69 @@ namespace RecipeBox
       Post["/ingredients/new"] = _ => {
         Ingredient newIngredient = new Ingredient(Request.Form["ingredient-name"]);
         newIngredient.Save();
+        return View["index.cshtml"];
+      };
+      Get["category/{id}"] = parameters => {
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        Category SelectedCategories = Category.Find(parameters.id);
+        List<Recipe> RecipeCategory = SelectedCategories.GetRecipes();
+        List<Recipe> AllRecipes = Recipe.GetAll();
+        model.Add("category", SelectedCategories);
+        model.Add("recipeCategory", RecipeCategory);
+        model.Add("allRecipes", AllRecipes);
+        return View["category.cshtml", model];
+      };
+      Post["category/success"] = _ => {
+        Category category = Category.Find(Request.Form["category-id"]);
+        Recipe recipe = Recipe.Find(Request.Form["recipe-id"]);
+        recipe.AddCategory(category);
+        return View["index.cshtml"];
+      };
+
+      Get["ingredient/{id}"] = parameters => {
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        Ingredient SelectedIngredient = Ingredient.Find(parameters.id);
+        List<Recipe> RecipeIngredient = SelectedIngredient.GetRecipes();
+        List<Recipe> AllRecipes = Recipe.GetAll();
+        model.Add("ingredient", SelectedIngredient);
+        model.Add("recipeIngredient", RecipeIngredient);
+        model.Add("allRecipes", AllRecipes);
+        return View["ingredient.cshtml", model];
+      };
+      Post["ingredient/success"] = _ => {
+        Ingredient ingredient = Ingredient.Find(Request.Form["ingredient-id"]);
+        Recipe recipe = Recipe.Find(Request.Form["recipe-id"]);
+        recipe.AddIngredient(ingredient);
+        return View["index.cshtml"];
+      };
+
+      Get["recipe/{id}"] = parameters => {
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        Recipe SelectedRecipe = Recipe.Find(parameters.id);
+        List<Ingredient> RecipeIngredients = SelectedRecipe.GetIngredients();
+        List<Ingredient> AllIngredients = Ingredient.GetAll();
+        List<Category> RecipeCategories = SelectedRecipe.GetCategories();
+        List<Category> AllCategories = Category.GetAll();
+        model.Add("recipe", SelectedRecipe);
+        model.Add("recipeIngredients", RecipeIngredients);
+        model.Add("recipeCategories", RecipeCategories);
+        model.Add("allIngredients", AllIngredients);
+        model.Add("allCategories", AllCategories);
+        return View["recipe.cshtml", model];
+      };
+      Post["recipe/ingredient/success"] = _ => {
+        Ingredient ingredient = Ingredient.Find(Request.Form["ingredient-id"]);
+        Recipe recipe = Recipe.Find(Request.Form["recipe-id"]);
+        recipe.AddIngredient(ingredient);
         return View["success.cshtml"];
       };
+      Post["recipe/category/success"] = _ => {
+        Category category = Category.Find(Request.Form["category-id"]);
+        Recipe recipe = Recipe.Find(Request.Form["recipe-id"]);
+        recipe.AddCategory(category);
+        return View["success.cshtml"];
+      };
+
     }
   }
 }
